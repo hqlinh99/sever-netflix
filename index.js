602e6d51782fc6ce4dba61703d17b150be50c303
Linh Hoang <hqlinh040599@gmail.com>
2021-12-27 20:36:36 +0700

    make it better

--- index.js
+++ index.js
@@ -32,11 +32,11 @@ app.use("/api/users", userRoute);
 app.use("/api/movies", movieRoute);
 app.use("/api/lists", listRoute);
 
-// app.use(express.static(path.join(__dirname, "/client/build")));
+app.use(express.static(__dirname, "/client/build"));
 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
-// });
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
+});
 
 app.listen(process.env.PORT || 8000, () => {
   console.log("Sever is running!");
