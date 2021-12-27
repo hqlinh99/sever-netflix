4fe58391b651e22df571c7279504d77fae8c8928
Linh Hoang <hqlinh040599@gmail.com>
2021-12-27 21:13:43 +0700

    make it better

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
 const path = require("path"); 
-const path = require("path"); 
 
 app.use(cookieParser());
 
