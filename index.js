5068eb0541880ec3a0e042ad2878d4b4fa0bf54e
Linh Hoang <hqlinh040599@gmail.com>
2021-12-27 20:30:05 +0700

    make it better

--- index.js
+++ index.js
@@ -34,9 +34,9 @@ app.use("/api/lists", listRoute);
 
 // app.use(express.static(path.join(__dirname, "/client/build")));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
+// });
 
 app.listen(process.env.PORT || 8000, () => {
   console.log("Sever is running!");
