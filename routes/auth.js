65d2840f38b82adcfa5f6ffecee17c9abd08f797
Linh Hoang <hqlinh040599@gmail.com>
2021-12-28 15:42:05 +0700

    make it better

--- routes/auth.js
+++ routes/auth.js
@@ -53,7 +53,7 @@ router.post("/login", async (req, res) => {
   );
   res.cookie("token", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
+    secure: true,
     maxAge: 60 * 60 * 1000,
   });
   const { password, ...info } = user._doc;
